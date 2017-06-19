1f224f003dea6f8b86702d15e3de8938f510cf9c
simonbigredtiger <simon@bigredtiger.com>
2017-06-19 00:18:41 -0400

    Added error route

--- app/components/App/AppTpl.js
+++ app/components/App/AppTpl.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import ToDo from '../../components/ToDo/ToDoContainer';
 import About from '../../components/About/AboutTpl';
 import Nav from '../../components/Nav/NavContainer';
+import Oops from '../../components/App/Oops';
 import './App.less';
 
 export default function AppTpl(props) {
@@ -14,6 +15,7 @@ export default function AppTpl(props) {
 				<Route exact path="/" component={ToDo}/>
 				<Route path="/purple" component={ToDo}/>
 				<Route path="/white" component={About}/>
+				<Route path="*" component={Oops}/>
 			</Switch>
 		</div>
 	);
---
+++ app/components/App/Oops.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Oops() {
+	return (
+		<section className="item error">
+			<div className="title">Oops!</div>
+			Looks like this is not a valid route.
+		</section>
+	);
+}
